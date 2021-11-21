16e02d18cbe7a73dad9982b2c1fe9510090872b4
diegobaena89 <diegobf.cello@gmail.com>
2021-11-21 20:53:08 -0300

    models added

--- index.js
+++ index.js
@@ -1,25 +1,75 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const flash = require('express-flash');
 
 const app = express();
 
-const authRoutes = require('./routes/authRoutes');
+const conn = require('./db/conn');
+
+// models
+const Collection = require('./models/Collection');
+const User = require('./models/User');
 
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
+app.use(express.json());
+
+// session middleware
+app.use(
+  session({
+    name: 'session',
+    secret: 'our_secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require('path').join(require('os').tmpdir(), 'sessions'),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true,
+    },
+  }),
+);
+
+// flash messages
+app.use(flash());
+
+// public path
 app.use(express.static('public'));
+
+// set session to res
+app.use((req, res, next) => {
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
+
+  next();
+});
 
 app.get('/registrar', (req, res) => {
   res.render('registrar');
 });
 
-app.get('/', authRoutes);
-
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.listen(3000, () => {
-  console.log('Conectado!');
-});
+conn
+  // .sync({ force: true })
+  .sync()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
---
+++ models/Collection.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+
+const db = require('../db/conn');
+
+const User = require('./User');
+
+// User
+
+const Collection = db.define('Collection', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    require: true,
+  },
+  author: {
+    type: DataTypes.STRING,
+    require: true,
+  },
+  image: {
+    type: DataTypes.STRING,
+    require: true,
+  },
+});
+
+Collection.belongsTo(User);
+User.hasMany(Collection);
+
+module.exports = Collection;
